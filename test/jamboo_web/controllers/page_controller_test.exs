64b6545d680961a4685ddb2e758ea27c6858b3f8
europa1988 <europa1988@bk.ru>
2026-07-13 16:02:21 +0300

    Delete test directory

--- test/jamboo_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule JambooWeb.PageControllerTest do
-  use JambooWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
